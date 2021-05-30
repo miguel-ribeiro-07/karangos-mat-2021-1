83a27f2f96e67bc5a86e49d9d449065a40c81875
miguel-ribeiro-07 <miguel.ribeiro.07@protonmail.com>
2021-05-30 16:30:49 +0000

    Layout Karangos

--- karangos/src/App.js
+++ karangos/src/App.js
@@ -1,37 +1,44 @@
-/*import logo from './logo.svg';
+/*
+import logo from './logo.svg';
 import './App.css';
-import Button from "@material-ui/core/Button"
+import Button from '@material-ui/core/Button'
 */
-  /*return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="contained" color="primary">Clique Aqui!</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );*/
-import TopBar from "./ui/TopBar"
-import FooterBar from "./ui/FooterBar";
+
+/*
+  <div className="App">
+    <header className="App-header">
+      <h1>Projeto Karangos</h1>
+      <Button variant="contained" color="primary">Clique aqui!</Button>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+*/
+
+
+
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import yellow from '@material-ui/core/colors/yellow';
 import red from '@material-ui/core/colors/red';
-import yellow from "@material-ui/core/colors/yellow";
-import { dark } from "@material-ui/core/styles/createPalette";
+import TopBar from './ui/TopBar'
+import FooterBar from './ui/FooterBar'
+import Box from '@material-ui/core/Box'
+import { makeStyles } from '@material-ui/core/styles'
 
 const theme = createMuiTheme({
   palette: {
+    type: 'dark',
     primary: {
-      type:dark,
       main: yellow[500],
     },
     secondary: {
@@ -39,13 +46,31 @@ const theme = createMuiTheme({
     },
   },
 });
+
+const useStyles = makeStyles((theme) => ({
+  main: {
+    backgroundColor: theme.palette.background.default,
+    minHeight: '100vh', // 100% da altura da área de visualização
+    margin: '0 0 0 0'  // margem de 42px inferior para acomodar a barra inferior
+  }
+}))
+
+function Main() {
+  const classes = useStyles()
+  return (
+    <Box className={classes.main}>
+      <TopBar />
+      <FooterBar />
+    </Box>
+  )
+}
+
 function App() {
-  return(<div>
+  return (
     <ThemeProvider theme={theme}>
-    <TopBar/>
-    <FooterBar></FooterBar>
+      <Main />
     </ThemeProvider>
-    </div>)
+  );
 }
 
 export default App;
